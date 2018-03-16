e153e5d39d98f0f27d97409d71c7b6f1107a7212
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-03-16 05:41:37 -0400

    Don't need the same recursive remove procedures as used in the homework repo

--- Remove.sh
+++ Remove.sh
@@ -1,5 +1,2 @@
-# Delete files recursively (for nested directories), thanks to:
-# https://unix.stackexchange.com/questions/116389/recursively-delete-all-files-with-a-given-extension
-
-find . -type f -name '*~' -delete
-find . -type f -name '*\#*' -delete
+-rm -f *~
+ -rm -f *\#*
